15ec3c73df5eabba19048c61536d0a3734ebcb23
一步上九霄 <3237928406@qq.com>
2020-10-08 08:39:04 +0800

    创建工程

---
+++ cmake-build-debug/CMakeFiles/F12.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/F12.dir/main.c.obj"
+  "F12.exe"
+  "F12.exe.manifest"
+  "F12.pdb"
+  "libF12.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/F12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
